9b3e8e64f0498400e60ebaede8c78153d22d0755
known <mondo.beyondo3@gmail.com>
2024-10-17 18:02:09 -0400

    Move resource finding out into separate functions

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,14 @@
 
 use std::env;
 use std::fs::File;
-use std::io::{BufRead, Read, Write};
-use std::net::TcpListener;
+use std::io::{BufRead, BufWriter, Read, Write};
+use std::net::{TcpListener, TcpStream};
 
 use local_ip_address::local_ip;
 use qrcode::QrCode;
 
-fn main() {
-    let args: Vec<_> = env::args().collect();
-
-    if args.len() != 3 || !(&args[1] == "--port" || &args[1] == "-p") {
-        return;
-    }
-
-    let mut addr = local_ip().unwrap().to_string();
-    addr.push_str(":");
-    addr.push_str(&args[2]);
-
-    println!("{addr}");
-
-    let listener = TcpListener::bind(&addr).unwrap();
-
-    let code = QrCode::new(format!("http://{addr}")).unwrap();
+fn render_server_qr_code(addr: &str, port: &str) {
+    let code = QrCode::new(format!("http://{}", format!("{}:{}", addr, port))).unwrap();
 
     let qr_term = code
         .render()
@@ -32,8 +18,94 @@ fn main() {
         .dark_color("\u{001b}[1;34;37;40m \u{001b}[0m")
         .build();
 
-    println!("Server now running at http://{}", &addr);
+    println!("Server now running at http://{}:{}", addr, port);
     println!("{qr_term}");
+}
+
+fn mime_type(file: &str) -> Result<String, String> {
+    match file.split(".").last() {
+        Some("html") => Ok("text/html".to_string()),
+        Some("css") => Ok("text/css".to_string()),
+        Some("js") => Ok("text/javascript".to_string()),
+        None | Some(&_) => Err("Mime type could not be determined".to_string()),
+    }
+}
+
+fn retrieve_resource(locator: &str, stream: TcpStream) {
+    let mut resource_path = env::current_dir().unwrap();
+    resource_path.push(locator);
+
+    let resource = File::open(resource_path);
+
+    match resource {
+        Ok(mut f) => {
+            let mut resource_content = String::new();
+
+            f.read_to_string(&mut resource_content)
+                .expect("Could not read from resource");
+
+            let mut response = BufWriter::new(stream);
+
+            response.write("HTTP/1.1 200 OK\r\n".as_bytes()).unwrap();
+            response
+                .write_fmt(format_args!(
+                    "Content-type: {}\r\n",
+                    mime_type(locator.split("/").last().unwrap()).unwrap(),
+                ))
+                .unwrap();
+            response
+                .write_fmt(format_args!(
+                    "Content-length: {}\r\n\r\n",
+                    resource_content.len()
+                ))
+                .unwrap();
+            response.write(resource_content.as_bytes()).unwrap();
+        }
+        Err(_) => {
+            let mut resource_content = String::new();
+
+            File::open(format!(
+                "{}/resources/404.html",
+                env::current_dir().unwrap().to_str().unwrap()
+            ))
+            .unwrap()
+            .read_to_string(&mut resource_content)
+            .unwrap();
+
+            let mut response = BufWriter::new(stream);
+
+            response
+                .write("HTTP/1.1 404 Not Found\r\n".as_bytes())
+                .unwrap();
+            response
+                .write("Content-type: text/html\r\n".as_bytes())
+                .unwrap();
+            response
+                .write_fmt(format_args!(
+                    "Content-length: {}\r\n\r\n",
+                    resource_content.len()
+                ))
+                .unwrap();
+            response.write(resource_content.as_bytes()).unwrap();
+        }
+    }
+}
+
+fn main() {
+    let args: Vec<_> = env::args().collect();
+
+    if args.len() != 3 || !(&args[1] == "--port" || &args[1] == "-p") {
+        eprintln!("No port provided, please provide a port with -p or --port");
+        return;
+    }
+
+    let port = &args[2];
+    let addr = local_ip().unwrap().to_string();
+
+    let listener =
+        TcpListener::bind(format!("{}:{}", addr, port)).expect("Failed to initialize server");
+
+    render_server_qr_code(&addr, port);
 
     for stream in listener.incoming() {
         let mut inc = stream.unwrap();
@@ -54,24 +126,21 @@ fn main() {
 
         if method == "GET" {
             if resource == "/" {
-                let mut index_page = File::open(format!(
-                    "{}/resources/index.html",
-                    env::current_dir().unwrap().to_str().unwrap()
-                ))
-                .unwrap();
-
-                let mut index_content = String::new();
-
-                index_page.read_to_string(&mut index_content).unwrap();
-
-                inc.write(format!("HTTP/1.1 200 OK\r\n").as_bytes())
-                    .unwrap();
-                inc.write(format!("Content-Type: text/html; charset=UTF-8\r\n").as_bytes())
-                    .unwrap();
-                inc.write(format!("Content-Length: {}\r\n", index_content.len()).as_bytes())
-                    .unwrap();
-                inc.write(format!("\r\n").as_bytes()).unwrap();
-                inc.write(index_content.as_bytes()).unwrap();
+                retrieve_resource(
+                    &format!(
+                        "{}/resources/index.html",
+                        env::current_dir().unwrap().to_str().unwrap()
+                    ),
+                    inc,
+                );
+            } else if resource == "/chat" {
+                retrieve_resource(
+                    &format!(
+                        "{}/resources/chat.html",
+                        env::current_dir().unwrap().to_str().unwrap()
+                    ),
+                    inc,
+                );
             } else if resource == "/new-message" {
                 let new_message = "{\"message\": \"This is another message for you\"}";
 
@@ -84,6 +153,7 @@ fn main() {
                 inc.write(format!("\r\n").as_bytes()).unwrap();
                 inc.write(new_message.as_bytes()).unwrap();
             }
+        } else if method == "POST" {
         }
     }
 }
