222a768b0ed600cdfe20fd515cda26aad237610d
known <mondo.beyondo3@gmail.com>
2024-10-19 13:48:17 -0400

    Move server struct to its own file

--- src/main.rs
+++ src/main.rs
@@ -11,35 +11,6 @@ use std::path::PathBuf;
 use local_ip_address::local_ip;
 use qrcode::QrCode;
 
-struct Server {
-    listener: TcpListener,
-    static_files_dir: PathBuf,
-    routes: HashMap<String, String>,
-}
-
-impl Server {
-    fn new(address: &str, static_files_dir: Option<&str>) -> Self {
-        let mut routes = HashMap::new();
-
-        routes.insert("/".to_string(), "index.html".to_string());
-
-        Self {
-            listener: TcpListener::bind(address).expect("Error binding server to address"),
-            static_files_dir: static_files_dir
-                .unwrap_or(env::current_dir().unwrap().to_str().unwrap())
-                .parse()
-                .expect("Failed parsing static files directory"),
-            routes,
-        }
-    }
-
-    fn register_route(&mut self, route: String, resource: String) {
-        if !self.routes.contains_key(&route) {
-            self.routes.insert(route, resource);
-        }
-    }
-}
-
 fn render_server_qr_code(addr: &str, port: &str) {
     let code = QrCode::new(format!("http://{}", format!("{}:{}", addr, port))).unwrap();
 
---
+++ src/server.rs
@@ -0,0 +1,28 @@
+struct Server {
+    listener: TcpListener,
+    static_files_dir: PathBuf,
+    routes: HashMap<String, String>,
+}
+
+impl Server {
+    fn new(address: &str, static_files_dir: Option<&str>) -> Self {
+        let mut routes = HashMap::new();
+
+        routes.insert("/".to_string(), "index.html".to_string());
+
+        Self {
+            listener: TcpListener::bind(address).expect("Error binding server to address"),
+            static_files_dir: static_files_dir
+                .unwrap_or(env::current_dir().unwrap().to_str().unwrap())
+                .parse()
+                .expect("Failed parsing static files directory"),
+            routes,
+        }
+    }
+
+    fn register_route(&mut self, route: String, resource: String) {
+        if !self.routes.contains_key(&route) {
+            self.routes.insert(route, resource);
+        }
+    }
+}
