11dc4f253c2e05a8b133b74c508bcf41f8e18e4f
known <mondo.beyondo3@gmail.com>
2025-05-08 23:49:55 -0400

    Update parsing to allow env file

--- src/option_parse.rs
+++ src/option_parse.rs
@@ -28,7 +28,7 @@ impl SmollChatOpts {
         let env_file = File::open(".env");
 
         match env_file {
-            Ok(file) => Ok(Self::parse_env(file)),
+            Ok(file) => Ok(Self::parse_env(file)?),
             Err(_) => Self::parse_args(env::args()),
         }
     }
@@ -37,12 +37,16 @@ impl SmollChatOpts {
         let mut opts_parsed = Self::default();
 
         while let Some(opt) = iter.next() {
-            match opt.as_str() {
+            match opt.as_ref() {
                 "--port" | "-p" => {
                     opts_parsed.port = match iter.next() {
-                        Some(a) => match a.parse::<u32>() {
+                        Some(a) => match a.as_ref().parse::<u32>() {
                             Ok(port) => port,
-                            Err(_) => return Err(error::OptionParsingError::InvalidValue(a)),
+                            Err(_) => {
+                                return Err(error::OptionParsingError::InvalidValue(
+                                    a.as_ref().to_string(),
+                                ))
+                            }
                         },
                         None => {
                             return Err(error::OptionParsingError::NoValueFound);
@@ -50,27 +54,44 @@ impl SmollChatOpts {
                     };
                 }
                 "--qrcode" => {
-                    opts_parsed.qrcode = iter
-                        .next()
-                        .expect("Not enough arguments passed")
-                        .parse::<bool>()
-                        .expect("Invalid boolean passed")
+                    opts_parsed.qrcode = match iter.next() {
+                        Some(v) => match v.as_ref().parse::<bool>() {
+                            Ok(value) => value,
+                            Err(_) => {
+                                return Err(error::OptionParsingError::InvalidValue(
+                                    v.as_ref().to_string(),
+                                ))
+                            }
+                        },
+                        None => {
+                            return Err(error::OptionParsingError::NoValueFound);
+                        }
+                    }
                 }
                 "--static-dir" => {
-                    opts_parsed.static_dir =
-                        PathBuf::from(iter.next().expect("Not enough arguments passed"))
+                    opts_parsed.static_dir = match iter.next() {
+                        Some(static_dir) => PathBuf::from(static_dir.as_ref()),
+                        None => return Err(error::OptionParsingError::NoValueFound),
+                    }
                 }
                 "--room-name" => {
-                    opts_parsed.room_name = iter.next().expect("Not enough arguments passed")
+                    opts_parsed.room_name = match iter.next() {
+                        Some(v) => v.as_ref().to_string(),
+                        None => return Err(error::OptionParsingError::NoValueFound),
+                    }
                 }
-                _ => return Err(error::OptionParsingError::InvalidKey(opt)),
+                _ => {
+                    return Err(error::OptionParsingError::InvalidKey(
+                        opt.as_ref().to_string(),
+                    ))
+                }
             }
         }
 
         Ok(opts_parsed)
     }
 
-    pub fn parse_args(mut args: env::Args) -> SmollParseResult {
+    pub fn parse_args(args: env::Args) -> SmollParseResult {
         Self::parse_iter(args)
     }
 
@@ -83,6 +104,54 @@ impl SmollChatOpts {
             .read_to_string(&mut buf)
             .expect("Error reading opts file");
 
-        Self::parse_iter(buf.split_whitespace())
+        let mut opts = buf.split_whitespace();
+
+        while let Some(opt) = opts.next() {
+            let mut pair = opt.split("=");
+            let key = pair.next().unwrap();
+            let value = pair.next();
+
+            match key {
+                "port" => {
+                    opts_parsed.port = match value {
+                        Some(v) => match v.parse::<u32>() {
+                            Ok(port) => port,
+                            Err(_) => {
+                                return Err(error::OptionParsingError::InvalidValue(v.to_string()));
+                            }
+                        },
+                        None => {
+                            return Err(error::OptionParsingError::NoValueFound);
+                        }
+                    }
+                }
+                "qrcode" => {
+                    opts_parsed.qrcode = match value {
+                        Some(v) => match v.parse::<bool>() {
+                            Ok(boolean) => boolean,
+                            Err(_) => {
+                                return Err(error::OptionParsingError::InvalidValue(v.to_string()))
+                            }
+                        },
+                        None => return Err(error::OptionParsingError::NoValueFound),
+                    }
+                }
+                "static-dir" => {
+                    opts_parsed.static_dir = match value {
+                        Some(v) => PathBuf::from(v),
+                        None => return Err(error::OptionParsingError::NoValueFound),
+                    }
+                }
+                "room-name" => {
+                    opts_parsed.room_name = match value {
+                        Some(v) => v.to_string(),
+                        None => return Err(error::OptionParsingError::NoValueFound),
+                    }
+                }
+                _ => return Err(error::OptionParsingError::InvalidKey(key.to_string())),
+            }
+        }
+
+        return Ok(opts_parsed);
     }
 }
